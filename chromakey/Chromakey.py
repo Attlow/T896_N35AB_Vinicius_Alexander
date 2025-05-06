5cbfadab24e748c66940d9912f7c18751400bd26
Attlow <vinicius.alexander2004@gmail.com>
2025-05-06 19:14:18 -0300

    Final

--- chromakey/Chromakey.py
+++ chromakey/Chromakey.py
@@ -3,46 +3,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 imgverde = cv2.imread('chromakey/img_fundo_verde_1.jpg')
-# Converte a s cores para LAB para melhor segmentação
 img_lab = cv2.cvtColor(imgverde, cv2.COLOR_BGR2LAB)
 
-# Extrai o canal 'a' (verde-magenta)
 canal_a = img_lab[:, :, 1]
 
-# Aplica limiarização com Otsu 
 _, thresh = cv2.threshold(canal_a, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-# Aplica a máscara à imagem original
 mascara = cv2.bitwise_and(imgverde, imgverde, mask=thresh)
 
-# Define pixels fora da máscara como brancos
 m1 = mascara.copy()
 m1[thresh == 0] = (255, 255, 255)
 
-# Converte as cores para LAB para melhor segmentação
 mlab = cv2.cvtColor(mascara, cv2.COLOR_BGR2LAB)
 
-# Normaliza o canal 'a'
 dst = cv2.normalize(mlab[:, :, 1], dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
-# Aplica outro threshold para reduzir ruído 
 _, dst_thresh = cv2.threshold(dst, 100, 255, cv2.THRESH_BINARY_INV)
 
-# Remove ruído alterando o canal 'a'
 mlab2 = mlab.copy()
 mlab2[:, :, 1][dst_thresh == 255] = 127
 
-# Converte de volta para BGR
 img_sem_fundo = cv2.cvtColor(mlab2, cv2.COLOR_LAB2BGR)
 
-# Define fundo como branco
 img_sem_fundo[thresh == 0] = (255, 255, 255)
 
 background = cv2.imread('chromakey/background_1.png')
-# Redimensiona o fundo para combinar com a imagem original
 background = cv2.resize(background, (imgverde.shape[1], imgverde.shape[0]))
 
-# Cria a imagem final
 mask_inv = cv2.bitwise_not(thresh)
 fundo_area = cv2.bitwise_and(background, background, mask=mask_inv)
 frente_area = cv2.bitwise_and(img_sem_fundo, img_sem_fundo, mask=thresh)
--- circulos/Deteccao_Circulos.py
+++ circulos/Deteccao_Circulos.py
@@ -2,37 +2,29 @@ import cv2
 import matplotlib.pyplot as plt 
 import numpy as np
 
-# Carrega a imagem
-img = cv2.imread('/home/vinicius/Python/PDI/circulos/circulos_1.png')
+img = cv2.imread('circulos\circulos_1.png')
 copia = img.copy()
-
-# Converte para tons de cinza
 img_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Detecta círculos com Hough
 circulos = cv2.HoughCircles(
     img_cinza,
     cv2.HOUGH_GRADIENT,
     dp=1.2,
-    minDist=80,       # Maior distância entre centros
-    param1=100,       # Threshold alto para borda (Canny)
-    param2=50,        # Threshold mais alto para o centro — menos falsos
-    minRadius=40,     # Ajuste com base no tamanho real das fichas
+    minDist=80,       
+    param1=100,       
+    param2=50,       
+    minRadius=40,    
     maxRadius=60
 )
 
-
-
-# Se houver círculos, desenha
 if circulos is not None:
     circulos = np.round(circulos[0, :]).astype("int")
 
     for (x, y, r) in circulos:
-        cv2.circle(copia, (x, y), r, (0, 255, 0), 4)  # Círculo
-        cv2.rectangle(copia, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)  # Centro
+        cv2.circle(copia, (x, y), r, (0, 255, 0), 4)  # 
+        cv2.rectangle(copia, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)  
 
-# Exibe imagens
-plt.figure(figsize=(12, 5))
+plt.figure(figsize=(8, 5))
 
 plt.subplot(1, 2, 1)
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
